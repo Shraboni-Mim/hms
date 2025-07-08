94f292787a3d2b2f129430f77e0be50460db9973
Md. Hasibul Islam <126506689+Shraboni-Mim@users.noreply.github.com>
2025-07-09 00:53:35 +0600

    appoinment feature

---
+++ appt_input.php
@@ -0,0 +1,7 @@
+<form action="appt.php" method="POST">
+    Phone: <input type="text" name="phone"><br>
+    Name: <input type="text" name="name"><br>
+    Date: <input type="text" name="date"><br>
+    Doctor: <input type="text" name="doctor"><br>
+    <input type="submit" value="Make Appointment">
+</form>
